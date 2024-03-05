a7571abf444e7d8609b94a064ea1eaa89b298c2c
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-03-05 19:19:13 +0000

    made a js webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: David.E
+// Created on: March 5, 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "Hey, how's your day going"
+}
+
